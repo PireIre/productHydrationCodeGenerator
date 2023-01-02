f999e3a8cda333131e2ac8d4f0af32c8c2418137
Irenej <irenej@bambuser.com>
2023-01-02 14:26:48 +0100

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.1ef1c58f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproduct_hydration_generator=self.webpackChunkproduct_hydration_generator||[]).push([[5539],{2689:function(e){function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.1ef1c58f.chunk.js.map
